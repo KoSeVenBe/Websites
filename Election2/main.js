b7914ec78dbd230af6dee734f43098af3180a36a
Mark Delchev <markdelchev@gmail.com>
2023-03-21 22:51:51 +0200

    different sound depending on candidate

--- Election2/main.js
+++ Election2/main.js
@@ -1,5 +1,15 @@
+
+
 function playAudioBSP(url) {
     var audio = document.getElementById("audioBSP");
+    audio.play();
+    setTimeout(function() {
+      window.location.href = url;
+    }, audio.duration * 1000);
+  }
+  
+  function playAudioISTINATA(url) {
+    var audio = document.getElementById("audioISTINATA");
     audio.play();
     setTimeout(function() {
       window.location.href = url;
