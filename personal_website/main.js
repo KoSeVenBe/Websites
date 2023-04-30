69331a2bd9daed2100f92bc2d8bf209764b129ee
mark-delchev <markdelchev@gmail.com>
2023-04-30 23:11:29 +0300

    personal website better dropdown menu

--- personal_website/main.js
+++ personal_website/main.js
@@ -1,3 +1,13 @@
 setTimeout(function() {
     document.querySelector('.main').classList.add('show');
   }, 1000); /* delay in milliseconds */
+
+function toggleDropdown() {
+  var dropdown = document.getElementById("dropdown-menu");
+  if (dropdown.style.display === "block") {
+    dropdown.style.display = "none";
+  } else {
+    dropdown.style.display = "block";
+    dropdown.classList.add("dropdown-active");
+  }
+}
